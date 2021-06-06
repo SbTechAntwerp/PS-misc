149b9b6429bd7fcb5b2814ba75b38cf6f4f0d03c
Shloima Berger <Shloima.berger@student.ap.be>
2021-06-06 20:38:42 +0200

    adde dthe first script with the history scripts

---
+++ enabling_ssh-agent_commands.ps1
@@ -0,0 +1,43 @@
+Set-Location ".\OneDrive - Artesis Plantijn Hogeschool Antwerpen\2-sem2-OsScripting\PS-misc"
+cls
+get-help | Get-Member
+Get-Command -name 
+Get-Command -name execution*
+Get-Command -name *-Execution*
+Get-ExecutionPolicy
+Get-Member -InputObject "this string"
+Get-Process | Select-Object -First 5
+Get-Process | Select-Object |Get-Member
+Select-Object |Get-Member
+Select-Object |Get-Member
+Get-Process | Select-Object -SkipLast
+get-help where-obk
+get-help Where-Object -Examples
+get-help Where-Object -Examples -InformationAction
+get-help Where-Object -Examples
+get-help foreach-Object -Examples
+$args = args
+foreach($args in $args);
+
+
+
+args = $args
+ForEach-Object $arg(in $_. in args)
+echo $args
+
+
+
+
+ls
+git pull
+.\first-script.ps1 ab cgde jldfhkj fjm
+$args
+.\first-script.ps1 ab cgde jldfhkj fjm
+.\first-script.ps1 ab cgde jldfhkj fjm
+Get-PSDrive
+git fetch
+git fetch
+Get-History | Set-Content ~enabling_ssh-agent_commands.ps1
+Get-History | Set-Content ~/enabling_ssh-agent_commands.ps1
+Get-History | Set-Content ~/enabling_ssh-agent_commands.ps1
+Get-History | Set-Content /enabling_ssh-agent_commands.ps1
